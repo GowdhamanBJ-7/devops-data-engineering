80d7e2b93eb0c185fa82ea5c63b9d9a337e5fa81
GowdhamanBJ-7 <gowdhaman.bj@diggibyte.com>
2026-02-28 17:24:19 +0530

    commit: transformation

--- notebooks/gold_aggregate.py
+++ notebooks/gold_aggregate.py
@@ -2,10 +2,17 @@ from pyspark.sql import functions as F
 
 silver_df = spark.table("workspace.default.sales_silver")
 
-gold_df = (
-    silver_df
-    .groupBy("customer_id")
-    .agg(F.sum("amount").alias("total_amount"))
+gold_df = silver_df.withColumn(
+    "age_group",
+    F.when(F.col("Age") < 30, "Young")
+     .otherwise("Senior")
 )
-
-gold_df.write.format("delta").mode("overwrite").saveAsTable("workspace.default.sales_gold")
+df_gold_summary = (
+    gold_df
+    .groupBy("age_group")
+    .agg(
+        F.count("*").alias("total_people"),
+        F.round(F.avg("Age"), 2).alias("avg_age")
+    )
+)
+df_gold_summary.write.mode("overwrite").saveAsTable("workspace.default.sales_gold")
--- notebooks/silver_transform.py
+++ notebooks/silver_transform.py
@@ -1,10 +1,16 @@
 from pyspark.sql import functions as F
+from pyspark.sql.window import Window
 
 bronze_df = spark.table("workspace.default.sales_bronze")
 
 silver_df = (
     bronze_df
-    .dropDuplicates()
+    .withColumn("last_name", F.trim(F.split(F.col("Name"), ",")[0]))
+    .withColumn("first_name", F.trim(F.split(F.col("Name"), ",")[1]))
+    .drop("Name")
 )
+window_spec = Window.orderBy(F.monotonically_increasing_id())
 
-silver_df.write.format("delta").mode("overwrite").saveAsTable("workspace.default.sales_silver")
+df_silver = silver_df.withColumn("id", F.row_number().over(window_spec))
+
+df_silver.write.mode("overwrite").saveAsTable("workspace.default.sales_silver")
