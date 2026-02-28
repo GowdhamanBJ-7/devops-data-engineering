8f0e1b2c22bfd41e1e79c3e18a2c31294b0911f9
GowdhamanBJ-7 <gowdhaman.bj@diggibyte.com>
2026-02-28 13:35:38 +0530

    datasource update

--- notebooks/job_notebook.py
+++ notebooks/job_notebook.py
@@ -3,7 +3,7 @@ from src.transformation import transform
 
 spark = SparkSession.builder.getOrCreate()
 
-df = spark.read.csv("/FileStore/raw_data.csv", header=True, inferSchema=True)
+df = spark.read.csv("/Volumes/customer_order_details/customer/customer_volume/20240105_sales_customer.csv", header=True, inferSchema=True)
 
 final_df = transform(df)
 
