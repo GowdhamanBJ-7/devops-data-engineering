1fcaf478ff9451edde3b737a1b8f332b6fa481f3
GowdhamanBJ-7 <gowdhaman.bj@diggibyte.com>
2026-02-28 15:59:45 +0530

    terraform cluster update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,53 +1,56 @@
 resource "databricks_job" "medallion_pipeline" {
   name = "medallion-prod-pipeline"
 
+  
+  # SCHEDULE (Runs every 10 hours IST)
   schedule {
-    quartz_cron_expression = "0 0 * * * ?"
+    quartz_cron_expression = "0 0 0/10 * * ?"
     timezone_id            = "Asia/Kolkata"
   }
 
-  job_cluster {
-    job_cluster_key = "medallion_cluster"
-
-    new_cluster {
-      num_workers   = 1
-      spark_version = "13.3.x-scala2.12"
-      node_type_id  = "Standard_DS3_v2"
-    }
-  }
-
+  # BRONZE TASK
   task {
     task_key = "bronze_task"
-    job_cluster_key = "medallion_cluster"
 
     notebook_task {
       notebook_path = "/Shared/devops_demo/bronze_ingest"
     }
+
+    # Serverless compute
+    environment_key = "serverless"
   }
 
+  
+  # SILVER TASK
   task {
     task_key = "silver_task"
+
     depends_on {
       task_key = "bronze_task"
     }
 
-    job_cluster_key = "medallion_cluster"
-
     notebook_task {
       notebook_path = "/Shared/devops_demo/silver_transform"
     }
+
+    # Serverless compute
+    environment_key = "serverless"
   }
 
+  # GOLD TASK
   task {
     task_key = "gold_task"
+
     depends_on {
       task_key = "silver_task"
     }
 
-    job_cluster_key = "medallion_cluster"
-
     notebook_task {
       notebook_path = "/Shared/devops_demo/gold_aggregate"
     }
+
+    # Serverless compute
+    environment_key = "serverless"
   }
 }
+
