3984419de7cdcedc6d404e97ce11a7ee4ce95acc
GowdhamanBJ-7 <gowdhaman.bj@diggibyte.com>
2026-02-28 16:03:29 +0530

    add Terraform medallion workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,17 @@
 resource "databricks_job" "medallion_pipeline" {
   name = "medallion-prod-pipeline"
 
-  
-  # SCHEDULE (Runs every 10 hours IST)
+  ############################################################
+  # SCHEDULE (Daily midnight IST)
+  ############################################################
   schedule {
-    quartz_cron_expression = "0 0 0/10 * * ?"
+    quartz_cron_expression = "0 0 0 * * ?"
     timezone_id            = "Asia/Kolkata"
   }
 
+  ############################################################
   # BRONZE TASK
+  ############################################################
   task {
     task_key = "bronze_task"
 
@@ -16,12 +19,14 @@ resource "databricks_job" "medallion_pipeline" {
       notebook_path = "/Shared/devops_demo/bronze_ingest"
     }
 
-    # Serverless compute
-    environment_key = "serverless"
+    compute {
+      serverless {}
+    }
   }
 
-  
+  ############################################################
   # SILVER TASK
+  ############################################################
   task {
     task_key = "silver_task"
 
@@ -33,11 +38,14 @@ resource "databricks_job" "medallion_pipeline" {
       notebook_path = "/Shared/devops_demo/silver_transform"
     }
 
-    # Serverless compute
-    environment_key = "serverless"
+    compute {
+      serverless {}
+    }
   }
 
+  ############################################################
   # GOLD TASK
+  ############################################################
   task {
     task_key = "gold_task"
 
@@ -49,8 +57,8 @@ resource "databricks_job" "medallion_pipeline" {
       notebook_path = "/Shared/devops_demo/gold_aggregate"
     }
 
-    # Serverless compute
-    environment_key = "serverless"
+    compute {
+      serverless {}
+    }
   }
 }
-
