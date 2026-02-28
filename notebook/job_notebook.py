597539605d810d4fd6da7ee1e6dc90a8e00cc6c1
GowdhamanBJ-7 <gowdhaman.bj@diggibyte.com>
2026-02-28 13:30:24 +0530

    catalog update

--- notebook/job_notebook.py
+++ notebook/job_notebook.py
@@ -7,4 +7,4 @@ df = spark.read.csv("/FileStore/raw_data.csv", header=True, inferSchema=True)
 
 final_df = transform(df)
 
-final_df.write.format("delta").mode("overwrite").saveAsTable("sales_cleaned")
+final_df.write.format("delta").mode("overwrite").saveAsTable("workspace.default.devops_table")
