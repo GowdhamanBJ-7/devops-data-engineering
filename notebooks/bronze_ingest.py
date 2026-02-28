560f133fc05e4e081b2ae57deaccf65b216ffacb
GowdhamanBJ-7 <gowdhaman.bj@diggibyte.com>
2026-02-28 17:06:29 +0530

    commit: file path updated

--- notebooks/bronze_ingest.py
+++ notebooks/bronze_ingest.py
@@ -5,4 +5,4 @@ spark = SparkSession.builder.getOrCreate()
 df = spark.read.csv("/Volumes/my_catalog/source/folder_managed_files/people_basic_csv/", header=True)
 
 
-df.write.format("delta").mode("overwrite").saveAsTable("workspace.default.devops_table")
+df.write.format("delta").mode("overwrite").saveAsTable("workspace.default.sales_bronze")
