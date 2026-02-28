686c221aa334e83657925f451e4a806460b5313d
GowdhamanBJ-7 <gowdhaman.bj@diggibyte.com>
2026-02-28 17:12:03 +0530

    commit: file path updated

--- notebooks/silver_transform.py
+++ notebooks/silver_transform.py
@@ -5,7 +5,6 @@ bronze_df = spark.table("workspace.default.sales_bronze")
 silver_df = (
     bronze_df
     .dropDuplicates()
-    .filter(F.col("amount").isNotNull())
 )
 
 silver_df.write.format("delta").mode("overwrite").saveAsTable("workspace.default.sales_silver")
