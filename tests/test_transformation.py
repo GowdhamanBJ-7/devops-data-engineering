feb151e0a8eae8375e7a860ac03a433220b06a1c
GowdhamanBJ-7 <gowdhaman.bj@diggibyte.com>
2026-03-02 12:07:54 +0530

    commit: updated unit testing

--- tests/test_transformation.py
+++ tests/test_transformation.py
@@ -1,16 +1,38 @@
 import pytest
 from pyspark.sql import SparkSession
-from src.transformation import transform
+
+from notebooks.silver_transform import transform_silver
+from notebooks.gold_aggregate import transform_gold
 
 @pytest.fixture(scope="session")
 def spark():
-    return SparkSession.builder.master("local[1]").appName("test").getOrCreate()
+    spark = SparkSession.builder.master("local[1]").appName("test").getOrCreate()
+    yield spark
+    spark.stop()
 
-def test_transform(spark):
-    data = [(1, 100), (2, None), (1, 100)]
-    df = spark.createDataFrame(data, ["id", "amount"])
-    
-    result = transform(df)
-    
-    assert result.count() == 1
-    assert "amount_with_tax" in result.columns
+
+def test_transform_silver(spark):
+    data = [("John, Doe", "25")]
+    columns = ["Name", "Age"]
+    bronze_df = spark.createDataFrame(data, columns)
+
+    silver_df = transform_silver(bronze_df)
+    result = silver_df.collect()
+
+    assert "first_name" in silver_df.columns
+    assert "last_name" in silver_df.columns
+    assert "id" in silver_df.columns
+    assert result[0]["first_name"] == "Doe"
+
+# Assertions - It checks whether the transformation created these new columns.
+
+def test_transform_gold(spark):
+    data = [("Doe", "John", "25", "abc")]
+    columns = ["last_name", "first_name", "Age", "id"]
+    silver_df = spark.createDataFrame(data, columns)
+
+    gold_df = transform_gold(silver_df)
+    result = gold_df.collect()
+
+    assert result[0]["age_group"] == "Young"
+    assert isinstance(result[0]["Age"], int)
