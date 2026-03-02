1a51534335ad36715759e39f897890d207f067c5
GowdhamanBJ-7 <gowdhaman.bj@diggibyte.com>
2026-03-02 12:30:54 +0530

    commit: updated unit testing

--- notebooks/gold_aggregate.py
+++ notebooks/gold_aggregate.py
@@ -1,6 +1,15 @@
-
-from notebooks.utils.gold import transform_gold
+from pyspark.sql import functions as F
 
 silver_df = spark.table("workspace.default.sales_silver")
-gold_df = transform_gold(silver_df)
+
+gold_df = (
+    silver_df
+    .withColumn("Age", F.col("Age").cast("int"))
+    .withColumn(
+        "age_group",
+        F.when(F.col("Age") < 30, "Young")
+         .otherwise("Senior")
+    )
+)
+
 gold_df.write.mode("overwrite").saveAsTable("workspace.default.sales_gold")
--- notebooks/silver_transform.py
+++ notebooks/silver_transform.py
@@ -1,7 +1,17 @@
-from notebooks.utils.silver import transform_silver
-
+from pyspark.sql import functions as F
 
 bronze_df = spark.table("workspace.default.sales_bronze")
-silver_df = transform_silver(bronze_df)
 
-silver_df.write.mode("overwrite").option('overwriteschema','true').saveAsTable("workspace.default.sales_silver")
+silver_df = (
+    bronze_df
+    .withColumn("last_name", F.trim(F.split(F.col("Name"), ",")[0]))
+    .withColumn("first_name", F.trim(F.split(F.col("Name"), ",")[1]))
+    .drop("Name")
+)
+
+df_silver = silver_df.withColumn(
+    "id",
+    F.sha2(F.concat_ws("||", F.col("first_name"), F.col("last_name")), 256)
+)
+
+df_silver.write.mode("overwrite").option('overwriteschema','true').saveAsTable("workspace.default.sales_silver")
